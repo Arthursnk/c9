fb7f0c1a39df114d48c36f0387a33f6d7f53ca76
Arthursnk <120285369+Arthursnk@users.noreply.github.com>
2022-12-10 15:36:00 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var square
+function setup() {
+  createCanvas(600,600);
+  square = createSprite(50,100,50,50);
+}
+
+function draw() 
+{
+  background('red');
+//console.log('arthur');
+drawSprites();
+}
+
+
+
+
